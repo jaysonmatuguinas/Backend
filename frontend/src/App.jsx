0d5ced0d9f0775c85f756a0aa8ebf3e267fb37bd
jaysonmatuguinas <matuguinasjayson15@gmail.com>
2024-09-10 16:57:22 +0800

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
     return (
-        <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis rem ducimus blanditiis saepe sequi sit nihil modi, quidem officia totam?</div>
+        <div></div>
     )
 }
 
