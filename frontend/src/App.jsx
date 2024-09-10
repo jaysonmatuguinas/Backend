f7366a62c9a299f4225b966a7e74d5929b89c6cf
Jayson S. Matuguinas <jsmatuguinas@mapua.edu.ph>
2024-09-10 16:47:55 +0800

    update frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis rem ducimus blanditiis saepe sequi sit nihil modi, quidem officia totam?</div>
+    )
+}
+
+export default App
